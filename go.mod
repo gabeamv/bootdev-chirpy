44c39d0708a738fe5d01473e83630b862cd7f1eb
gabeamv <gabrielvillanueva11155@gmail.com>
2026-01-23 20:21:48 -0800

    initial webpage

---
+++ go.mod
@@ -0,0 +1,3 @@
+module bootdev-chirpy
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/assets", http.FileServer(http.Dir("./assets")))
+	server := &http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(fmt.Errorf("error listening on server: %w", err))
+	}
+
+}
